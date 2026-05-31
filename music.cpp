5c221f21c5a380d86e101c4eca1cc05a1899e339
tazu-22 <tazrinshemonty@gmail.com>
2026-06-01 01:34:34 +0600

    Final Update

--- music.cpp
+++ music.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
 #include<queue>
+#include <string>
+
 using namespace std;
 int main(){
-    
+    queue <string> playlist;
+    int choice;
+    string song;
+    do{
+        cout<<"\n Playlist\n";
+        cout<<"1.Add\n";
+        cout<<"2.Play\n";
+        cout<<"3.Current\n";
+        cout<<"4.Show\n";
+        cout<<"5.Total songs\n";
+        cout<<"6.Exit\n";
+        cout<<"Enter choice:";
+        cin>>choice;
+        switch(choice){
+            case 1:
+            cin.ignore();
+            cout<<"Enter song name:";
+            getline(cin,song);
+            playlist.push(song);
+            cout<<"Added\n";
+            break;
+            case 2:
+            if(playlist.empty()){
+                cout<<"Playlist empty\n";
+            }
+            else {
+                cout<<"Now playing"<<playlist.front()<<endl;
+                playlist.pop();
+            }
+            break;
+            case 3:
+             if(playlist.empty()){
+                cout<<"Playlist empty\n";
+            }
+            else {
+                cout<<"Next song:"<<playlist.front()<<endl;
+            }
+            break;
+            case 4:
+             if(playlist.empty()){
+                cout<<"Playlist empty\n";
+            }
+            else{
+                queue <string> temp= playlist;
+                cout<<"\n Playlist:\n";
+                while (!temp.empty()){
+                    cout<<temp.front()<<endl;
+                    temp.pop();
+                }
+            }
+            break;
+            case 5:
+            cout<<"Total:"<<playlist.size<<endl;
+            break;
+            case 6:
+            cout<<"Exiting.....\n";
+            break;
+            default:
+            cout<<"Invalid choice\n";
+        }
+
+    }   while(choice !=6);
+    return 0; 
 }
--- ticket.cpp
+++ ticket.cpp
@@ -1,6 +1,80 @@
 #include <iostream>
 #include<queue>
+#include <string>
+
 using namespace std;
 int main(){
+    queue <string> customers;
+    int choice;
+    string name;
     
+    do{
+        cout<<"\nMenu\n";
+        cout<< "1. Join\n";
+        cout<<"2. serve\n";
+        cout<<"3.Next\n";
+        cout<<"4.Count\n";
+        cout<<"5.Display Queue\n";
+        cout<< "6.Exit\n";
+        cout<<"Enter choice:";
+        cin>>choice;
+
+        switch(choice){
+
+            case 1:
+                cin.ignore();
+                cout<<"Enter name:";
+                getline(cin,name);
+
+                customers.push(name);
+                cout<<name<<" joined\n";
+                break;
+
+            case 2:
+            if(customers.empty()){
+                cout<<"No customers\n";
+            }
+            else{
+                cout<<customers.front()<<" got the ticket";
+                customers.pop();
+            }
+            break;
+            case 3:
+            if(customers.empty()){
+                cout<<"No customers\n";
+            }
+            else{
+                cout<<"Next customer:"<<customers.front()<<endl;
+            }
+            break;
+            case 4:
+            cout<<"Total customers:"<<customers.size()<<endl;
+            break;
+            case 5:
+            if(customers.empty()){
+                cout<<"No customers\n";
+            }
+            else{
+                queue <string> temp = customers;
+                cout<<"Queue:\n";
+                while(!temp.empty()){
+                    cout<<temp.front();
+
+                    temp.pop();
+                     if(!temp.empty()){
+                        cout<<"->";
+                     }
+                     cout<<endl;
+                    
+                }
+            }
+            break;
+            case 6:
+            cout<<"Exting.....\n";
+            break;
+            default:
+            cout<<"Invalid choice\n";
+        }
+    }while(choice !=6);
+    return 0;
 }
